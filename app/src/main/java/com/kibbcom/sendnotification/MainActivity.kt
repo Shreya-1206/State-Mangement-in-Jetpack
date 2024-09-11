151dfabde6537bc0e38e49aade9cb95c33eac85d
Shreya-1206 <shreyaray.1206@gmail.com>
2024-09-11 13:16:26 +0530

    second-commit

--- app/src/main/java/com/kibbcom/sendnotification/MainActivity.kt
+++ app/src/main/java/com/kibbcom/sendnotification/MainActivity.kt
@@ -5,11 +5,15 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MailOutline
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -17,35 +21,39 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
-import com.kibbcom.sendnotification.ui.theme.SendNotificationTheme
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-          sendNotification()
+            var counter :MutableState<Int> =  remember { mutableStateOf(0) }
+          Column {
+              sendNotification(counter.value, {counter.value++})   /// here we add the parameter 1 and second is lamdba and used it in composable to invoke this lambda
+              cardNotification(counter.value)
+          }
         }
     }
 }
 
 
 
-
-@Preview(showBackground = true)
 @Composable
 
-fun sendNotification () {
-    var counter :MutableState<Int> =  remember { mutableStateOf(0) }
+fun sendNotification (counter :Int, increment : () -> Unit) {
+
 //    Log.d("TAGGED", "Intial render")
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "You have sent ${counter.value} notifications")
+        Text(text = "You have sent ${counter} notifications")
         Button(
             onClick = {
-                counter.value++
+                 increment()
                 Log.d("TAGGED" , "intial render and on button click render")
             }) {
 
@@ -54,4 +62,24 @@ fun sendNotification () {
         }
     }
 
+}
+
+
+@Composable
+
+fun cardNotification(counter :Int) {
+    Card (
+        elevation = CardDefaults.cardElevation(9.dp)
+    ){
+        Row (
+            modifier = Modifier
+                .padding(horizontal = 10.dp, vertical = 7.dp)
+        ){
+            Image(
+                imageVector =Icons.Outlined.MailOutline,
+                contentDescription = "Message box"
+            )
+            Text(text = "There are $counter messages sent")
+        }
+    }
 }
